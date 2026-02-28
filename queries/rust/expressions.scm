219573b1122cabac1138dffe50128ed0ff1695a4
Searidang Pa <dangsyncpa@gmail.com>
2026-02-28 08:02:29 -0500

    if-rust-func

--- queries/rust/expressions.scm
+++ queries/rust/expressions.scm
@@ -219,3 +219,114 @@
                     field: (field_identifier) @func_name))
               ])))))
   ])
+
+(let_condition
+  value: [
+    (call_expression
+      function: [
+        (identifier) @func_name
+        (scoped_identifier
+          name: (identifier) @func_name)
+        (field_expression
+          field: (field_identifier) @func_name)
+        (generic_function
+          function: (identifier) @func_name)
+        (generic_function
+          function: (scoped_identifier
+            name: (identifier) @func_name))
+        (generic_function
+          function: (field_expression
+            field: (field_identifier) @func_name))
+      ])
+    (try_expression
+      (call_expression
+        function: [
+          (identifier) @func_name
+          (scoped_identifier
+            name: (identifier) @func_name)
+          (field_expression
+            field: (field_identifier) @func_name)
+          (generic_function
+            function: (identifier) @func_name)
+          (generic_function
+            function: (scoped_identifier
+              name: (identifier) @func_name))
+          (generic_function
+            function: (field_expression
+              field: (field_identifier) @func_name))
+        ]))
+    (await_expression
+      (call_expression
+        function: [
+          (identifier) @func_name
+          (scoped_identifier
+            name: (identifier) @func_name)
+          (field_expression
+            field: (field_identifier) @func_name)
+          (generic_function
+            function: (identifier) @func_name)
+          (generic_function
+            function: (scoped_identifier
+              name: (identifier) @func_name))
+          (generic_function
+            function: (field_expression
+              field: (field_identifier) @func_name))
+        ]))
+    (try_expression
+      (await_expression
+        (call_expression
+          function: [
+            (identifier) @func_name
+            (scoped_identifier
+              name: (identifier) @func_name)
+            (field_expression
+              field: (field_identifier) @func_name)
+            (generic_function
+              function: (identifier) @func_name)
+            (generic_function
+              function: (scoped_identifier
+                name: (identifier) @func_name))
+            (generic_function
+              function: (field_expression
+                field: (field_identifier) @func_name))
+          ])))
+    (call_expression
+      function: (field_expression
+        value: (await_expression
+          (call_expression
+            function: [
+              (identifier) @func_name
+              (scoped_identifier
+                name: (identifier) @func_name)
+              (field_expression
+                field: (field_identifier) @func_name)
+              (generic_function
+                function: (identifier) @func_name)
+              (generic_function
+                function: (scoped_identifier
+                  name: (identifier) @func_name))
+              (generic_function
+                function: (field_expression
+                  field: (field_identifier) @func_name))
+            ]))))
+    (try_expression
+      (call_expression
+        function: (field_expression
+          value: (await_expression
+            (call_expression
+              function: [
+                (identifier) @func_name
+                (scoped_identifier
+                  name: (identifier) @func_name)
+                (field_expression
+                  field: (field_identifier) @func_name)
+                (generic_function
+                  function: (identifier) @func_name)
+                (generic_function
+                  function: (scoped_identifier
+                    name: (identifier) @func_name))
+                (generic_function
+                  function: (field_expression
+                    field: (field_identifier) @func_name))
+              ])))))
+  ])
